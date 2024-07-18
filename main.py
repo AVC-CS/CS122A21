be7efa4a0b5b61b403bff7d1941ab31922dc72bd
Kyuwoong Lee <kwooler@gmail.com>
2024-07-18 07:25:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     """
     ##################################################
     Comlete your code here
-    Use m_perc and f_perc for your results
+    Use m_perc, f_perc, nb_perc for your results
+    m_perc = the percentage of male 
+    f_perc = the percentage of female 
+    nb_perc = the percentage of others
     ##################################################
     """
 
@@ -11,7 +14,7 @@ def main():
     # Do not delete the return statement
     ########################################
     """
-    return m_perc, f_perc
+    return m_perc, f_perc, nb_perc
 
 
 if __name__ == '__main__':
